70dfc88d0f1fc86a8f6da4c5cfff56516c2c0772
Samuel Medina <smdoomed@gmail.com>
2021-11-30 12:30:50 -0600

    [add] Global styles and color palette

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle `
+    :root {
+        --primary-color: #2f2c52;
+        --secondary-color: #3e3b64;
+        --tertiary-color: #5665fb;
+        --orange: #fd6c22;
+        --white: #ffffff;
+    }
+    body {
+        background-color: var(--primary-color);
+    }
+
+`
+
+export default GlobalStyles;
